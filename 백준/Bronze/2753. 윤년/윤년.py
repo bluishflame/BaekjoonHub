9c05fc561627978286d7d9c6559d45395889e557
YeonLog <122681003+bluishflame@users.noreply.github.com>
2025-01-31 10:29:31 +0900

    Update 윤년.py

--- 백준/Bronze/2753. 윤년/윤년.py
+++ 백준/Bronze/2753. 윤년/윤년.py
@@ -5,3 +5,13 @@ if ((year%4==0) and (year%100!=0) or (year%400==0)) :
 
 else : 
     print("0")
+
+
+'''
+# 삼항 연산자 풀이
+
+year = int(input())
+
+print('1') if ((year%4 == 0)and(year%100 != 0)) or (year%400 == 0) else print('0')
+
+'''
