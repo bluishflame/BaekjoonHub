91a9ae487838405d177753013fc25a462db8f675
YeonLog <122681003+bluishflame@users.noreply.github.com>
2025-01-31 10:28:10 +0900

    Update 검증수.py

--- 백준/Bronze/2475. 검증수/검증수.py
+++ 백준/Bronze/2475. 검증수/검증수.py
@@ -1,3 +1,16 @@
 a, b, c, d, e = map(int, input().split())
 
 print((a**2+b**2+c**2+d**2+e**2)%10)
+
+
+'''
+num = map(int, input().split())
+
+result = 0
+
+for i in num:
+   result += i ** 2
+
+print(result%10)
+
+'''
