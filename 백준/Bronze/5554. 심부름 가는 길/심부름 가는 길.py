ca5dd7b0dcb6380617500840c9ab1c34eb2e83b9
YeonLog <122681003+bluishflame@users.noreply.github.com>
2025-02-03 14:47:06 +0900

    [Bronze IV] Title: 심부름 가는 길, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/5554. 심부름 가는 길/심부름 가는 길.py
@@ -0,0 +1,12 @@
+num1 = int(input())
+num2 = int(input())
+num3 = int(input())
+num4 = int(input())
+
+total_time = num1 + num2 + num3 + num4
+
+minutes = total_time // 60  
+seconds = total_time % 60   
+
+print(minutes)
+print(seconds)
